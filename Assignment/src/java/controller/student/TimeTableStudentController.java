7740b58571126ea95a957a564d27b8b75afb06bc
Doan-x <tdoan0587@gmail.com>
2024-03-13 00:05:07 +0700

    Update TimeTableStudentController.java

--- Assignment/src/java/controller/student/TimeTableStudentController.java
+++ Assignment/src/java/controller/student/TimeTableStudentController.java
@@ -5,6 +5,7 @@
 package controller.student;
 
 import controller.account.BaseRequiredAuthenticationController;
+import controller.authentication.authorization.BaseRBACController;
 import dal.LessionDBContext;
 import dal.LoginDBContext;
 import dal.SlotDBContext;
@@ -20,6 +21,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import model.Account;
 import model.Lession;
+import model.Role;
 import model.Student;
 import model.TimeSlot;
 import util.DateTimeHelper;
@@ -29,15 +31,15 @@ import util.DateTimeHelper;
  * @author Admin
  */
 @WebServlet(name = "TimeTableStudentController", urlPatterns = {"/student/timetable"})
-public class TimeTableStudentController extends BaseRequiredAuthenticationController {
+public class TimeTableStudentController extends BaseRBACController {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp, Account account, ArrayList<Role> roles) throws ServletException, IOException {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp, Account account, ArrayList<Role> roles) throws ServletException, IOException {
         SlotDBContext sdb = new SlotDBContext();
         ArrayList<TimeSlot> slots = sdb.list();
 
@@ -45,15 +47,21 @@ public class TimeTableStudentController extends BaseRequiredAuthenticationContro
         String to_raw = req.getParameter("to");
         java.sql.Date from = null;
         java.sql.Date to = null;
-        
-        LoginDBContext login = new LoginDBContext();
-        int sid = login.getStudentByUserName(account.getUsername()).getSid();
-        
+
+        String sid_raw = req.getParameter("sid").trim();
+        int sid = -1;
+        if (sid_raw == null || sid_raw.isEmpty()) {
+            LoginDBContext login = new LoginDBContext();
+            sid = login.getLecturerByUserName(account.getUsername()).getLid();
+        } else {
+            sid = Integer.parseInt(sid_raw);
+        }
+
         Date today = new Date();
         if (from_raw == null || from_raw.isEmpty()) {
             from = DateTimeHelper.convertUtilDateToSqlDate(DateTimeHelper.addDaysToDate(DateTimeHelper.getWeekStart(today), 1));
         } else {
-            from = java.sql.Date.valueOf(from_raw);
+            from = java.sql.Date.valueOf(from_raw.trim());
         }
 
         if (to_raw == null || to_raw.isEmpty()) {
@@ -70,13 +78,15 @@ public class TimeTableStudentController extends BaseRequiredAuthenticationContro
         LessionDBContext ldb = new LessionDBContext();
         ArrayList<Lession> les = ldb.getLessionBySid(sid, from, to);
         
-        
-        
+        StudentDBContext stdb = new StudentDBContext();
+        Student student = stdb.getStudentBySid(sid);
+
         req.setAttribute("slots", slots);
         req.setAttribute("dates", dates);
         req.setAttribute("from", from);
         req.setAttribute("to", to);
         req.setAttribute("lession", les);
+        req.setAttribute("student", student);
         req.setAttribute("account", account);
         req.getRequestDispatcher("../view/student/stimetable.jsp").forward(req, resp);
     }
